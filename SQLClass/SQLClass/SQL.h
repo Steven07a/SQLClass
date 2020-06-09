ee029e18ff5181d21813f164add46f5bb926091b
Steven Herrera <stevenherrera70396@gmail.com>
2020-06-09 07:48:30 -0700

    project mostly wokring now need to add in comments

--- SQLClass/SQLClass/SQL.h
+++ SQLClass/SQLClass/SQL.h
@@ -55,7 +55,7 @@ void SQL::save_files() {
 
 void SQL::run() {
 	string input = "";
-	cout << ">";
+	cout << "SQL> ";
 	getline(cin, input);
 	if (input.compare(0, 5, "batch") == 0) {
 		batchRun();
@@ -85,71 +85,89 @@ void SQL::batchRun() {
 				p.set_string(charInput);
 				charInput[0] = '\0';
 				ptree = p.parse_tree();
-				if (debug) {
-					cout << "\n\n\n\n";
-					ptree.print_table();
-					cout << "\n\n\n\n";
-				}
-				if (ptree.contains("where")) {
-					if (ptree["command"][0] == "select") {
-						cout << "\n[" << commandCount++ << "] " << ptree["command"][0]
-							<< " " << ptree["fields"] << "from " << ptree["table"][0]
-							<< " where " << ptree["conditions"] << endl;
-						Table temp = table[ptree["table"][0]].select(
-							ptree["fields"],
-							ptree["conditions"]);
-						cout << temp;
-						cout << "\n";
-					} else if (ptree["command"][0] == "delete") {
-						if (table.contains(ptree["table"][0])) {
-							table[ptree["table"][0]].remove(ptree["conditions"]);
-						}
+				if (!ptree.empty()) {
+					if (debug) {
+						cout << "\n\n\n\n";
+						ptree.print_table();
+						cout << "\n\n\n\n";
 					}
-				} else {
-					if (ptree["command"][0] == "create") {
-						table.insert(ptree["table"][0], Table(ptree["table"][0], ptree["fields"]));
-						if (!fileNames.contains(ptree["table"][0])) {
-							fileNames.push_back(ptree["table"]);
+					if (ptree.contains("where")) {
+						if (fileNames.contains(ptree["table"][0])) {
+							if (ptree["command"][0] == "select") {
+								cout << "\n[" << commandCount++ << "] " << ptree["command"][0]
+									<< " " << ptree["fields"] << "from " << ptree["table"][0]
+									<< " where " << ptree["conditions"] << endl;
+								Table temp = table[ptree["table"][0]].select(
+									ptree["fields"],
+									ptree["conditions"]);
+								cout << temp;
+								cout << "\n";
+							} else if (ptree["command"][0] == "delete") {
+								if (table.contains(ptree["table"][0])) {
+									table[ptree["table"][0]].remove(ptree["conditions"]);
+								
+									cout << "\n[" << commandCount++ << "] " << ptree["command"][0]
+										<< " " << ptree["fields"] << "from " << ptree["table"][0]
+										<< " where " << ptree["conditions"] << endl;
+								}
+							}
 						}
+					} else {
+						if (ptree["command"][0] == "create") {
+							table.insert(ptree["table"][0], Table(ptree["table"][0], ptree["fields"]));
+							if (!fileNames.contains(ptree["table"][0])) {
+								fileNames.push_back(ptree["table"]);
+							}
 
-						cout << "[" << commandCount++ << "] create table "
-							<< ptree["table"][0] << " cols: " << ptree["fields"]
-							<< "\nTable " << ptree["table"][0] << " created. cols: " 
-							<< ptree["fields"] << endl;
+							cout << "[" << commandCount++ << "] create table "
+								<< ptree["table"][0] << " cols: " << ptree["fields"]
+								<< "\nTable " << ptree["table"][0] << " created. cols: "
+								<< ptree["fields"] << endl;
 
-					}else if (ptree["command"][0] == "make") {
-						table.insert(ptree["table"][0], Table(ptree["table"][0], ptree["fields"]));
-						if (!fileNames.contains(ptree["table"][0])) {
-							fileNames.push_back(ptree["table"]);
-						}
+						} else if (ptree["command"][0] == "make") {
+							table.insert(ptree["table"][0], Table(ptree["table"][0], ptree["fields"]));
+							if (!fileNames.contains(ptree["table"][0])) {
+								fileNames.push_back(ptree["table"]);
+							}
 
-						cout << "[" << commandCount++ << "] make table "
-							<< ptree["table"][0] << " cols: " << ptree["fields"]
-							<< "\nTable " << ptree["table"][0] << " created. cols: "
-							<< ptree["fields"] << endl;
+							cout << "[" << commandCount++ << "] make table "
+								<< ptree["table"][0] << " cols: " << ptree["fields"]
+								<< "\nTable " << ptree["table"][0] << " created. cols: "
+								<< ptree["fields"] << endl;
 
-					} else if (ptree["command"][0] == "insert") {
-						table[ptree["table"][0]].insert_into(ptree["fields"]);
-						cout << "[" << commandCount++ << "] insert into " << ptree["table"][0]
-							<< " values: " << ptree["fields"] << "\n";
+						} else if (ptree["command"][0] == "insert") {
+							if (fileNames.contains(ptree["table"][0])) {
+								table[ptree["table"][0]].insert_into(ptree["fields"]);
+								cout << "[" << commandCount++ << "] insert into " << ptree["table"][0]
+									<< " values: " << ptree["fields"] << "\n";
 
-					} else if (ptree["command"][0] == "select") {
-						if (ptree["fields"][0] == "*") {
-							cout << "\n[" << commandCount++ << "] " << ptree["command"] 
-								<< " " << ptree["fields"][0] << " from " << ptree["table"] << endl;
-							Table temp = table[ptree["table"][0]].select_all();
-							cout << temp;
-							cout << endl;
-						} else {
-							cout << "\n[" << commandCount++ << "] " << ptree["command"]
-								<< " " << ptree["fields"][0] << " from " << ptree["table"] << endl;
-							Table temp = table[ptree["table"][0]].select(ptree["fields"]);
-							cout << temp << endl;
-						}
-					} else if (ptree["command"][0] == "drop") {
-						if (table.contains(ptree["table"][0])) {
-							table.erase(ptree["command"][0]);
-							fileNames.remove(ptree["table"][0]);
+							}
+						} else if (ptree["command"][0] == "select") {
+							if (fileNames.contains(ptree["table"][0])) {
+								if (ptree["fields"][0] == "*") {
+									cout << "\n[" << commandCount++ << "] " << ptree["command"]
+										<< " " << ptree["fields"][0] << " from " << ptree["table"] << endl;
+									Table temp = table[ptree["table"][0]].select_all();
+									cout << temp;
+									cout << endl;
+								} else {
+									cout << "\n[" << commandCount++ << "] " << ptree["command"]
+										<< " " << ptree["fields"][0] << " from " << ptree["table"] << endl;
+									Table temp = table[ptree["table"][0]].select(ptree["fields"]);
+									cout << temp << endl;
+								}
+							}
+						} else if (ptree["command"][0] == "drop") {
+							if (fileNames.contains(ptree["table"][0])) {
+								if (table.contains(ptree["table"][0])) {
+									table.erase(ptree["table"][0]);
+									fileNames.remove(ptree["table"][0]);
+								
+									cout << "\n[" << commandCount++ << "] " << ptree["command"][0]
+										<< "Table: " << ptree["table"][0]
+										<< endl;
+								}
+							}
 						}
 					}
 				}
@@ -165,8 +183,9 @@ void SQL::userInputRun(string in) {
 	bool debug = false;
 	
 	while (true) {
-		cout << "=====================SQL=====================\n";
+		
 		if (in.empty()) {
+			cout << "SQL> ";
 			getline(cin, input);
 		} else {
 			input = in;
@@ -191,46 +210,57 @@ void SQL::userInputRun(string in) {
 				ptree.print_table();
 				cout << "\n\n\n\n";
 			}
+			if (!ptree.empty()) {
+				if (ptree.contains("where")) {
+					if (ptree["command"][0] == "select") {
+						cout << "\n";
+						Table temp = table[ptree["table"][0]].select(
+							ptree["fields"],
+							ptree["conditions"]);
+						cout << temp;
+						cout << "\n";
+					} else if (ptree["command"][0] == "delete") {
+						if (table.contains(ptree["table"][0])) {
+							table[ptree["table"][0]].remove(ptree["conditions"]);
+						}
+					}
+				} else {
+					if (ptree["command"][0] == "create" || ptree["command"][0] == "make") {
+						table.insert(ptree["table"][0], Table(ptree["table"][0], ptree["fields"]));
+						if (!fileNames.contains(ptree["table"][0])) {
+							fileNames.push_back(ptree["table"]);
+						}
+					} else if (ptree["command"][0] == "insert") {
+						table[ptree["table"][0]].insert_into(ptree["fields"]);
+					} else if (ptree["command"][0] == "select") {
+						if (ptree["fields"][0] == "*") {
+							if (table.contains(ptree["table"][0])) {
+								cout << endl;
+								Table temp = table[ptree["table"][0]].select_all();
+								cout << temp;
+								cout << endl;
+							} else {
+								cout << "\nNo such table exist\n";
+							}
 
-			if (ptree.contains("where")) {
-				if (ptree["command"][0] == "select") {
-					cout << "\n";
-					Table temp = table[ptree["table"][0]].select(
-						ptree["fields"],
-						ptree["conditions"]);
-					cout << temp;
-					cout << "\n";
-				} else if (ptree["command"][0] == "delete") {
-					if (table.contains(ptree["table"][0])) {
-						table[ptree["table"][0]].remove(ptree["conditions"]);
+						} else {
+							cout << endl;
+							Table temp = table[ptree["table"][0]].select(ptree["fields"]);
+							cout << temp << endl;
+						}
+					} else if (ptree["command"][0] == "drop") {
+						if (table.contains(ptree["table"][0])) {
+							table.erase(ptree["table"][0]);
+							fileNames.remove(ptree["table"][0]);
+						} else {
+							cout << "\nTable " << ptree["table"][0] << " does not exist.\n";
+						}
 					}
 				}
 			} else {
-				if (ptree["command"][0] == "create") {
-					table.insert(ptree["table"][0], Table(ptree["table"][0], ptree["fields"]));
-					if (!fileNames.contains(ptree["table"][0])) {
-						fileNames.push_back(ptree["table"]);
-					}
-				} else if (ptree["command"][0] == "insert") {
-					table[ptree["table"][0]].insert_into(ptree["fields"]);
-				} else if (ptree["command"][0] == "select") {
-					if (ptree["fields"][0] == "*") {
-						cout << endl;
-						Table temp = table[ptree["table"][0]].select_all();
-						cout << temp;
-						cout << endl;
-					} else {
-						cout << endl;
-						Table temp = table[ptree["table"][0]].select(ptree["fields"]);
-						cout << temp << endl;
-					}
-				} else if (ptree["command"][0] == "drop") {
-					if (table.contains(ptree["table"][0])) {
-						table.erase(ptree["command"][0]);
-						fileNames.remove(ptree["table"][0]);
-					}
-				}
+				cout << "Invalid input.\n";
 			}
+			
 
 		}
 	}
@@ -238,7 +268,3 @@ void SQL::userInputRun(string in) {
 }
 
 #endif // !SQL_H_
-
-/*
-RUN BATCH simpletest.txt we are running into a problem at operation 32 and 33
-*/
